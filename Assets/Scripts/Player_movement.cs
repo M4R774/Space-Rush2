96d3f0d8624529a27e5cc63def194cbdf4bed098
Aleksi Martikainen <martta_1994@hotmail.com>
2015-06-30 10:08:36 +0300

    Luotu kansioita ja scene
    Siirretty assetteja omiin kansioihinsa, luotu pelaajalle placeholder
    grafiikka ja uusi scene asteroiditaistelua varten. Lisäksi tehty tyhjä
    scriptitiedosto pelaajan liikuttamista varten.

---
+++ Assets/Scripts/Player_movement.cs
@@ -0,0 +1,19 @@
+﻿// Liikuttaa pelaajan alusta taistelussa
+
+using UnityEngine;
+using System.Collections;
+
+public class Player_movement : MonoBehaviour {
+	
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+	}
+}
