2d037b1b0efdb8ac62f88b84a8c331eeb5bca3cc
Aleksi Martikainen <martta_1994@hotmail.com>
2015-06-30 17:49:45 +0300

    Lisättiin prefab asteroidi ja game_controller
    Prefab kansio jonka sisällä asteroidi prefabi. Lisäksi game_controller
    joka spawnaa asteroideja satunnaisesti. Game_controllerista voi
    määrittää asteroidiaaltojen spawnausta. Lisäksi luotiin uusi tag luokka
    hazards ja vaihdettiin pelaajan tagiksi Player.

---
+++ Assets/Scripts/Game_controller.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game_controller : MonoBehaviour {
+	
+    public GameObject[] hazards;
+	public Vector3 spawnValues;
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(SpawnWaves());
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                GameObject hazard = hazards[Random.Range(0, hazards.Length)];
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+        }
+    }
+}
