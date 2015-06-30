7e4d8c11195cd5c2131aa06e305507d2824a5010
Aleksi Martikainen <martta_1994@hotmail.com>
2015-06-30 14:44:03 +0300

    Pelaajaa voi nyt liikuttaa ja .apk sovellus luotu ja testattu
    Pelaajaa voi nyt liikuttaa taistelussa ja luotiin .apk tiedosto jonka
    pystyy todistetusti asentamaan omaan puhelimeensa.

--- Assets/Scripts/Player_movement.cs
+++ Assets/Scripts/Player_movement.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 using System.Collections;
 
 public class Player_movement : MonoBehaviour {
-	
+
+    // Muuttujia
+    public float Speed = 0f;
+    private float movex = 0f;
+    private float movey = 0f;
 
 	// Use this for initialization
 	void Start () {
@@ -13,7 +17,9 @@ public class Player_movement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
-
+        
+        movex = Input.GetAxis("Horizontal");
+        movey = Input.GetAxis("Vertical");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(movex * Speed, movey * Speed);
 	}
 }
