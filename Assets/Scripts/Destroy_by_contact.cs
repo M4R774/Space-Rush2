837547fd0fde2ce0f412b102cdba4cc290fba567
Aleksi Martikainen <martta_1994@hotmail.com>
2015-07-01 19:39:56 +0300

    Yritetty räpeltää animaatioita
    En mä vittu enää edes tiedä

---
+++ Assets/Scripts/Destroy_by_contact.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroy_by_contact : MonoBehaviour
+{
+    public GameObject explosion;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Boundary")
+        {
+            return;
+        }
+        Instantiate(explosion, transform.position, transform.rotation);
+
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
