2bc7efd0e82e1bbb422f846504796f55b3a4e4fd
Aleksi Martikainen <martta_1994@hotmail.com>
2015-07-01 10:17:50 +0300

    Objektit tuhoutuvat ruudun ulkopuolella ja uusi .apk buildi tehty
    Tippuvat asteroidit tuhoutuvat nyt tippuessaan ruudun ulkopuolelle.
    Lisäksi lisätty placholder taustakuva. Lisäksi tehty uusi .apk buildi
    joka testattu toimivaksi(liikettä puhelimen ruudulla).

---
+++ Assets/Scripts/Destroy_by_boundary.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroy_by_boundary : MonoBehaviour 
+{
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Destroy(other.gameObject);
+    }
+}
