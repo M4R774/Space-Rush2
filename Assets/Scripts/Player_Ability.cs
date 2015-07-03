b747a9ec25c8047c505adc38a3834de7d76ab959
Aleksi Martikainen <martta_1994@hotmail.com>
2015-07-03 20:59:48 +0300

    Ampumiselle ääni ja testailtu vähän miltä pikseligrafiikkanäyttäis
    Vähän raflan kanssa leikittii grafiikoilla ja äänillä. Lisäks tuli vähä
    tweakkailtua grafiikka-asetusten kanssa mikä johti jonkinasteiseen
    nykimiseen puhelimella. Täytyy tutkia miten saa siten että
    pikseligrafiikka ei ala näyttää paskalta blurrilta ja ei kuitenkaan
    lagaa.

--- Assets/Scripts/Player_Ability.cs
+++ Assets/Scripts/Player_Ability.cs
@@ -3,13 +3,17 @@ using System.Collections;
 
 public class Player_Ability : MonoBehaviour {
 
+
+
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+
+
 	}
 }
