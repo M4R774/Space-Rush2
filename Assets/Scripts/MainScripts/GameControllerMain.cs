e394865c8e7d2d7f62cc030a446628b8f74b3c1f
Quu-Ukko <quu.ukko@gmail.com>
2015-07-03 14:34:47 +0300

    Luotu meta scene
    Aloitin hahamottelemaan meta sceneä mietin vielä miten kameraa tulee
    liikuttaa ja miten pelaajaa olisi järkevä liikuttaa.

---
+++ Assets/Scripts/MainScripts/GameControllerMain.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControllerMain : MonoBehaviour {
+
+	public GameControllerMain instance = null;
+
+
+	// Kutsutaan kun scene käynnistyy
+	void Awake () {
+		// jos instancea ei ole alustettu niin tehdään se
+		if (instance == null)
+			instance = this;
+		
+		/*Jos instance on olemassa mutta se ei ole 
+		 * tämä olio niin tuhotaan se jotta useampia
+		 * GameControllereita ei pääse syntymään */
+		else if (instance != this)
+			Destroy(gameObject);
+		// Tämä mahdollistaa sen että controlleria ei tuhota kun palataan minipelistä takaisin metaan
+		DontDestroyOnLoad(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/MainScripts/MapController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapController : MonoBehaviour {
+	
+	public float speed = 0.01f;
+	private LineRenderer lRenderer;
+	
+	private int height = Screen.height;
+	private int width = Screen.width;
+
+	// Use this for initialization
+	void Awake () {
+
+		Vector3 start = new Vector3 ();
+		Vector3 end = new Vector3 ();
+
+		lRenderer = GetComponent<LineRenderer> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 offset = new Vector2(0,Time.time * speed);
+		
+		GetComponent<Renderer>().material.mainTextureOffset = offset;
+	}
+}
