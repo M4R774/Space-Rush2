4a5c5f2dc67f606e8b2c7e391f4503c57141ef7d
Aleksi Martikainen <martta_1994@hotmail.com>
2015-07-03 12:47:06 +0300

    Toimiva healthbar lisätty
    Pelaaja ottaa nyt damagea törmätessään asteroideihin. Damagen suuruuttaa
    voi säätää asteroidi prefabista.

--- Assets/Scripts/Destroy_by_contact.cs
+++ Assets/Scripts/Destroy_by_contact.cs
@@ -4,9 +4,24 @@ using System.Collections;
 public class Destroy_by_contact : MonoBehaviour
 {
     public GameObject explosion;
+	public float Damage;
+	GameObject Player;                          // Reference to the player GameObject.
+	Player_Health Health; 
+
+	void Awake ()
+	{
+		Player = GameObject.FindGameObjectWithTag ("Player");
+		Health = Player.GetComponent <Player_Health> ();
+	}
 
     void OnTriggerEnter2D(Collider2D other)
     {
+		if (other.tag == "Player")
+		{
+			Health.TakeDamage (Damage);
+			Instantiate(explosion, transform.position, transform.rotation);
+			Destroy(gameObject);
+		}
 		// Jos tagi on boundary tai pelaaja niin älä tuhoa
         if (other.tag == "Boundary" || other.tag == "Player")
         {
@@ -15,5 +30,5 @@ public class Destroy_by_contact : MonoBehaviour
         Instantiate(explosion, transform.position, transform.rotation);
         Destroy(other.gameObject);
         Destroy(gameObject);
-    }
+	}
 }
---
+++ Assets/Scripts/Player_Health.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Player_Health : MonoBehaviour 
+{
+	public float startHealth;
+	public float currentHealth;
+	public Slider Health_slider;
+
+	void Awake ()
+	{
+		currentHealth = startHealth;
+	}
+
+	public void TakeDamage (float damage)
+	{
+		currentHealth -= damage;
+		Health_slider.value = currentHealth;
+	}
+
+}
