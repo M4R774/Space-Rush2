5a7c369a749c5727801e4a39f948f76831b1c5fc
Quu-Ukko <quu.ukko@gmail.com>
2015-07-07 13:23:22 +0300

    Encountterit triggeraa
    Lisätty mainiin encountterit jotka nyt siirtävät pelajaan toiseen
    kenttään

---
+++ Assets/Scripts/MainScripts/EncounterScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using Random = UnityEngine.Random;
+
+public class EncounterScript : MonoBehaviour {
+
+	private string encounterName;
+	private string[] scenes = {"Asteroid_battle", "DefaultScene"};
+
+	// Use this for initialization
+	void Awake () 
+	{
+		encounterName = scenes[Random.Range(0, scenes.Length)];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//OnTriggerEnter2D is sent when another object enters a trigger collider attached to this object (2D physics only).
+	private void OnTriggerEnter2D (Collider2D other)
+	{
+		Application.LoadLevel(encounterName);
+	}
+
+
+}
--- Assets/Scripts/MainScripts/MapController.cs
+++ Assets/Scripts/MainScripts/MapController.cs
@@ -1,19 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Random = UnityEngine.Random;
 
 public class MapController : MonoBehaviour {
 	
 	public float speed = 0.01f;
-	private LineRenderer lRenderer;
-	
+	public GameObject encouter;
 	private int height = Screen.height;
 	private int width = Screen.width;
 
-	// Use this for initialization
-	void Awake () 
-	{
 
-		
+	
+	
+	// Use this for initialization
+	void Awake () {
+
+		Instantiate (encouter,new Vector3(2.75f, 2.75f, 0f), Quaternion.identity);
+		Instantiate (encouter,new Vector3(-2.75f, 2.75f, 0f), Quaternion.identity);
+
 	}
 	
 	// Update is called once per frame
