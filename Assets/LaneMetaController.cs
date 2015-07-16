6d669236473e66fd464f6abd9135b7ce505590b2
Aleksi Martikainen <martta_1994@hotmail.com>
2015-07-16 18:51:29 +0300

    Aloitettu tekemään uutta metaa
    Uudessa metassa on nyt 5 kaistaa joissa spawnaa randomilla juttuja.
    Vielä on tehtävä pelaajalle controllit, laitettava niin että törmäys
    triggeraa encountterin.

---
+++ Assets/LaneMetaController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaneMetaController : MonoBehaviour {
+	
+	public GameObject[] encounters;
+	public Vector3[] spawnValues;
+	public int encounterCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		StartCoroutine(SpawnWaves());
+	}
+	
+	IEnumerator SpawnWaves()
+	{
+		yield return new WaitForSeconds(startWait);
+		while (true)
+		{
+			for (int i = 0; i < encounterCount; i++)
+			{
+				GameObject hazard = encounters[Random.Range(0, encounters.Length)];
+				Vector3 spawnPosition = spawnValues[Random.Range (0, spawnValues.Length)];
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate(hazard, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds(spawnWait);
+			}
+			yield return new WaitForSeconds(waveWait);
+		}
+	}
+}
