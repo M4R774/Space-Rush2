421827396d1d2a59f556b341338d41307f2cc71a
Sauli Anto <sauli.anto@gmail.com>
2015-08-07 22:21:16 +0300

    Asetukset näkymä toteutettu

---
+++ Assets/Scripts/Menu/MainMenuScript.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEngine.UI; //Tarttee Ui-elementtien käyttöön
+using System.Collections;
+
+public class MainMenuScript : MonoBehaviour {
+
+	public Canvas quitMenu;
+	public Canvas optionsMenu;
+	public Button startText;
+	public Button exitText;
+	public Button optionsText;
+	public Text controlButton;
+
+	private bool touchControl = true;
+
+	void Awake () {
+		quitMenu = quitMenu.GetComponent<Canvas> ();
+		optionsMenu = optionsMenu.GetComponent<Canvas> ();
+		startText = startText.GetComponent<Button> ();
+		optionsText = optionsText.GetComponent<Button> ();
+		exitText = exitText.GetComponent<Button> ();
+		controlButton = controlButton.GetComponent<Text> ();
+		quitMenu.enabled = false; //QuitMenu pelin alkaessa piilossa
+	}
+
+	// Options menu pressed
+	public void OptionsPress() {
+		optionsMenu.enabled = true;
+		startText.enabled = false;
+		optionsText.enabled = false;
+	}
+
+	//Quit menu esiin, muiden nappien disablointi
+	public void ExitPress() {
+		quitMenu.enabled = true;
+		startText.enabled = false;
+		exitText.enabled = false;
+	}
+
+	public void NoPress() {
+		quitMenu.enabled = false;
+		optionsMenu.enabled = false;
+		startText.enabled = true;
+		optionsText.enabled = true;
+		exitText.enabled = true;
+	}
+
+	public void StartLevel() {
+		Application.LoadLevel ("Main");
+	}
+	public void Back() {
+		Application.LoadLevel (0);
+	}
+	public void Next() {
+		Application.LoadLevel (2);
+	}
+
+	public void ExitGame () {
+		Application.Quit ();
+	}
+
+	public void toggleControl() {
+		if (touchControl) {
+			touchControl = false;
+			controlButton.text = "Tilt";
+
+		} else {
+			touchControl = true;
+			controlButton.text = "Touch";
+		}
+	}
+}
