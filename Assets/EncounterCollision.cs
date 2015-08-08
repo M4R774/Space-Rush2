9881453c37270a69f16627e952f138c06ee725c5
Aleksi Martikainen <martta_1994@hotmail.com>
2015-08-08 05:49:32 +0300

    3LaneMeta vaihtaa nyt sceneä oikein
    I did it mom! Kun esim. törmää asteroidipilveen, alkaa Asteroid_battle
    scene jne.

---
+++ Assets/EncounterCollision.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EncounterCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.name == "AsteroidEncounter(Clone)")
+		{
+			Application.LoadLevel("Asteroid_battle");
+		}
+		if (other.gameObject.name == "RedNeckEncounter(Clone)")
+		{
+			Application.LoadLevel("Gunner");
+		}
+		if (other.gameObject.name == "KauppaAsemaEncounter(Clone)")
+		{
+			Application.LoadLevel("Store");
+		}
+	}
+}
