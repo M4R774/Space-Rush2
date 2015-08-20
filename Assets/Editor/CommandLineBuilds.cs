2db9b8d4ff2900f1ed7ac100f86378d6c0adb1e6
Sauli Anto <sauli.anto@gmail.com>
2015-08-20 22:02:18 +0300

    deleted uneeded files

---
+++ Assets/Editor/CommandLineBuilds.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public static class CommandLineBuilds {
+
+	static string[] GetScenePaths() {
+		string[] scenes = new string[EditorBuildSettings.scenes.Length];
+		
+		for(int i = 0; i < scenes.Length; i++)
+		{
+			scenes[i] = EditorBuildSettings.scenes[i].path;
+		}
+		
+		return scenes;
+	}
+	[MenuItem("File/AutoBuilder/Android")]
+	public static void BuildAndroid() {
+		EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTarget.Android);
+		BuildPipeline.BuildPlayer(GetScenePaths(), "android.apk", BuildTarget.Android, BuildOptions.None);
+	}
+}
