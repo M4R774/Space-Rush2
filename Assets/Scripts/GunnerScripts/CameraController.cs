47e5167098defe1139eb82f94cf931b51d26f6ce
Quu-Ukko <quu.ukko@gmail.com>
2015-12-26 13:21:51 +0200

    Parannettu kameran liikkeitä GunnerScenessa

--- Assets/Scripts/GunnerScripts/CameraController.cs
+++ Assets/Scripts/GunnerScripts/CameraController.cs
@@ -5,9 +5,9 @@ public class CameraController : MonoBehaviour {
 
 	public float distance = -10.0f;
 	public bool useCameraDistance = false;
-	public Vector2 startMarker;
-	public Vector2 endMarker;
-	public float speed = 1.0F;
+	public Vector3 startMarker;
+	public Vector3 endMarker;
+	public float speed = 1.0f;
 	public float journeyLength;
 
 	private Vector2 moveDistance;
@@ -29,17 +29,19 @@ public class CameraController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		if (Input.GetMouseButtonDown (0)) {
-			Vector3 mousePosition = Input.mousePosition;
-			//mousePosition.z = distance;
-
-			float distCovered = (Time.time - startTime) * speed;
-			float fracJourney = distCovered / journeyLength;
-
-			startMarker = Camera.main.transform.position;
-			endMarker = Camera.main.ScreenToWorldPoint (mousePosition);
-			transform.position = Vector2.Lerp(startMarker, endMarker, fracJourney);
-		}
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            startMarker = Camera.main.transform.position;
+            endMarker = Camera.main.ScreenToWorldPoint(mousePosition);
+            endMarker.z = -10;
+            journeyLength = Vector3.Distance(startMarker, endMarker);
+            float distCovered = (Time.time - startTime) * speed;
+            float fracJourney = distCovered / journeyLength;
+            transform.position = Vector3.Lerp(startMarker, endMarker, fracJourney);
+        }
+           
+		
 	}
 
 }
--- Assets/Scripts/GunnerScripts/GameController.cs
+++ Assets/Scripts/GunnerScripts/GameController.cs
@@ -22,7 +22,7 @@ public class GameController : MonoBehaviour {
 
 
 		endTime = Time.time + sceneLength;
-        StartCoroutine(SpawnWaves());
+     //   StartCoroutine(SpawnWaves());
 
 	}
 	
